0eb7b51f27bcc372a9b37dd408cfcd264831f5e4
Noa <coolreader18@gmail.com>
2023-03-02 23:23:49 -0600

    Rename file->path, dir->lib_path, add exclude option

--- derive-impl/src/compile_bytecode.rs
+++ derive-impl/src/compile_bytecode.rs
@@ -21,7 +21,6 @@ use rustpython_compiler_core::{frozen_lib, CodeObject, Mode};
 use std::{
     collections::BTreeMap,
     env, fs,
-    ops::Not,
     path::{Path, PathBuf},
 };
 use syn::{
@@ -42,12 +41,12 @@ fn resolve_path(path: &Path) -> std::borrow::Cow<'_, Path> {
 }
 
 enum CompilationSource {
-    /// Source is a File (Path)
-    File(PathBuf),
+    /// Source is a single module
+    Path(PathBuf),
     /// Direct Raw sourcecode
     SourceCode(String),
-    /// Source is a directory
-    Dir(PathBuf),
+    /// Source is a directory of modules
+    LibPath(PathBuf),
 }
 
 struct CompiledModule {
@@ -78,35 +77,28 @@ impl CompilationSource {
         &self,
         mode: Mode,
         module_name: String,
+        excludes: &[pattern::ModulePattern],
         compiler: &dyn Compiler,
     ) -> Result<Vec<(String, CompiledModule)>, String> {
+        let mut dir = DirWalker::new(excludes);
         match self {
-            CompilationSource::Dir(path) => DirWalker::from_dir(&resolve_path(path))?
-                .modules
-                .into_iter()
-                .map(|(module_name, (path, package))| {
-                    let module = Self::compile_file(&path, mode, &module_name, compiler)
-                        .map(|code| CompiledModule { code, package });
-                    (module_name, module)
-                })
-                .filter_map(|(module_name, res)| {
-                    let is_bad_syntax = res.is_err() && {
-                        let (parent, stem) =
-                            module_name.rsplit_once('.').unwrap_or(("", &module_name));
-                        // TODO: handle with macro arg rather than hard-coded path
-                        stem.starts_with("badsyntax_") || parent.ends_with(".encoded_modules")
-                    };
-                    is_bad_syntax.not().then(|| Ok((module_name, res?)))
-                })
-                .collect(),
-            _ => {
+            CompilationSource::LibPath(path) => dir.walk(&resolve_path(path), "")?,
+            CompilationSource::Path(path) => dir.add_entry(resolve_path(path).into(), "")?,
+            CompilationSource::SourceCode(_) => {
                 let module = CompiledModule {
                     code: self.compile_single(mode, &module_name, compiler)?,
                     package: false,
                 };
-                Ok(vec![(module_name, module)])
+                return Ok(vec![(module_name, module)]);
             }
         }
+        dir.modules
+            .into_iter()
+            .map(|(module_name, (path, package))| {
+                let code = Self::compile_file(&path, mode, &module_name, compiler)?;
+                Ok((module_name, CompiledModule { code, package }))
+            })
+            .collect()
     }
 
     fn compile_file(
@@ -145,28 +137,30 @@ impl CompilationSource {
         compiler: &dyn Compiler,
     ) -> Result<CodeObject, String> {
         match self {
-            CompilationSource::File(path) => Self::compile_file(path, mode, module_name, compiler),
+            CompilationSource::Path(path) => Self::compile_file(path, mode, module_name, compiler),
             CompilationSource::SourceCode(code) => {
                 Self::compile_string(&textwrap::dedent(code), mode, module_name, compiler)
                     .map_err(|err| format!("Python compile error in string literal: {err}"))
             }
-            CompilationSource::Dir(_) => {
-                unreachable!("Can't use compile_single with directory source")
+            CompilationSource::LibPath(_) => {
+                unreachable!("Can't use compile_single with lib source")
             }
         }
     }
 }
 
 #[derive(Default)]
-struct DirWalker {
+struct DirWalker<'a> {
+    excludes: &'a [pattern::ModulePattern],
     modules: BTreeMap<String, (PathBuf, bool)>,
 }
 
-impl DirWalker {
-    fn from_dir(path: &Path) -> Result<Self, String> {
-        let mut dir = Self::default();
-        dir.walk(path, "")?;
-        Ok(dir)
+impl<'a> DirWalker<'a> {
+    fn new(excludes: &'a [pattern::ModulePattern]) -> Self {
+        Self {
+            excludes,
+            modules: BTreeMap::new(),
+        }
     }
     fn walk(&mut self, path: &Path, parent: &str) -> Result<(), String> {
         let paths = fs::read_dir(path)
@@ -208,7 +202,9 @@ impl DirWalker {
                 [parent, ".", stem].concat()
             };
 
-            self.modules.insert(module_name, (path, is_init));
+            if !self.excludes.iter().any(|pat| pat.matches(&module_name)) {
+                self.modules.insert(module_name, (path, is_init));
+            }
         }
         Ok(())
     }
@@ -219,9 +215,10 @@ mod kw {
     syn::custom_keyword!(mode);
     syn::custom_keyword!(module_name);
     syn::custom_keyword!(source);
-    syn::custom_keyword!(file);
-    syn::custom_keyword!(dir);
+    syn::custom_keyword!(path);
+    syn::custom_keyword!(lib_path);
     syn::custom_keyword!(crate_name);
+    syn::custom_keyword!(exclude);
 }
 
 fn check_duplicate<T>(x: &Option<T>, span: Span) -> syn::Result<()> {
@@ -239,6 +236,7 @@ impl Parse for PyCompileArgs {
         let mut source: Option<CompilationSource> = None;
         let mut source_span = (Span::call_site(), Span::call_site());
         let mut crate_name = None;
+        let mut exclude = None;
 
         loop {
             if input.is_empty() {
@@ -267,24 +265,31 @@ impl Parse for PyCompileArgs {
                     source = Some(CompilationSource::SourceCode(code.value()));
                     source_span = (tok.span, code.span());
                 }
-                tok @ kw::file => {
+                tok @ kw::path => {
                     check_duplicate(&source, tok.span)?;
                     input.parse::<Token![=]>()?;
                     let path = input.call(parse_litstr)?;
-                    source = Some(CompilationSource::File(path.value().into()));
+                    source = Some(CompilationSource::Path(path.value().into()));
                     source_span = (tok.span, path.span());
                 }
-                tok @ kw::dir => {
+                tok @ kw::lib_path => {
                     check_duplicate(&source, tok.span)?;
                     input.parse::<Token![=]>()?;
                     let path = input.call(parse_litstr)?;
-                    source = Some(CompilationSource::Dir(path.value().into()));
+                    source = Some(CompilationSource::LibPath(path.value().into()));
                     source_span = (tok.span, path.span());
                 }
                 tok @ kw::crate_name => {
                     check_duplicate(&crate_name, tok.span)?;
                     input.parse::<Token![=]>()?;
                     crate_name = Some(input.call(parse_litstr)?.parse()?);
+                }
+                tok @ kw::exclude => {
+                    check_duplicate(&exclude, tok.span)?;
+                    input.parse::<Token![=]>()?;
+                    let content;
+                    syn::bracketed!(content in input);
+                    exclude = Some(content.parse_terminated(parse_litstr)?);
                 }
             });
 
@@ -307,6 +312,7 @@ impl Parse for PyCompileArgs {
             mode: mode.unwrap_or(Mode::Exec),
             module_name: module_name.unwrap_or_else(|| "frozen".to_owned()),
             crate_name: crate_name.unwrap_or_else(|| syn::parse_quote!(::rustpython_vm::bytecode)),
+            exclude: exclude.unwrap_or_default(),
         })
     }
 }
@@ -340,6 +346,7 @@ struct PyCompileArgs {
     mode: Mode,
     module_name: String,
     crate_name: syn::Path,
+    exclude: syn::punctuated::Punctuated<LitStr, Token![,]>,
 }
 
 pub fn impl_py_compile(
@@ -348,10 +355,10 @@ pub fn impl_py_compile(
 ) -> Result<TokenStream, Diagnostic> {
     let args: PyCompileArgs = parse2(input)?;
 
-    if matches!(args.source, CompilationSource::Dir(_)) {
+    if matches!(args.source, CompilationSource::LibPath(_)) {
         return Err(Diagnostic::spans_error(
             args.source_span,
-            "py_compile doesn't accept dir",
+            "py_compile doesn't accept lib",
         ));
     }
 
@@ -377,10 +384,16 @@ pub fn impl_py_freeze(
 ) -> Result<TokenStream, Diagnostic> {
     let args: PyCompileArgs = parse2(input)?;
 
+    let excludes = args
+        .exclude
+        .into_iter()
+        .map(|s| s.value().parse().map_err(|e| syn::Error::new(s.span(), e)))
+        .collect::<Result<Vec<_>, _>>()?;
+
     let crate_name = args.crate_name;
     let code_map = args
         .source
-        .compile(args.mode, args.module_name, compiler)
+        .compile(args.mode, args.module_name, &excludes, compiler)
         .map_err(|msg| Diagnostic::spans_error(args.source_span, msg))?;
 
     let data = frozen_lib::FrozenLib::encode(code_map.iter().map(|(k, v)| {
@@ -397,4 +410,142 @@ pub fn impl_py_freeze(
     };
 
     Ok(output)
+}
+
+mod pattern {
+    pub struct ModulePattern {
+        tokens: Vec<Token>,
+    }
+
+    #[derive(Copy, Clone, Debug)]
+    enum Token {
+        DoubleStar,
+        Star,
+        Char(char),
+    }
+
+    #[derive(Debug)]
+    pub enum PatternError {
+        BadDoubleStar,
+    }
+    impl std::fmt::Display for PatternError {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                PatternError::BadDoubleStar => {
+                    f.write_str("`**` must be alone in a path component")
+                }
+            }
+        }
+    }
+
+    impl std::str::FromStr for ModulePattern {
+        type Err = PatternError;
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut chars = s.chars().peekable();
+            let mut was_dot = true;
+            let tokens = std::iter::from_fn(|| {
+                chars.next().map(|c| match c {
+                    '*' if chars.peek() == Some(&'*') => {
+                        chars.next();
+                        if was_dot && matches!(chars.next(), None | Some('.')) {
+                            Ok(Token::DoubleStar)
+                        } else {
+                            Err(PatternError::BadDoubleStar)
+                        }
+                    }
+                    '*' => Ok(Token::Star),
+                    c => {
+                        was_dot = c == '.';
+                        Ok(Token::Char(c))
+                    }
+                })
+            });
+            let tokens = tokens.collect::<Result<_, _>>()?;
+            Ok(Self { tokens })
+        }
+    }
+
+    impl ModulePattern {
+        pub fn matches(&self, s: &str) -> bool {
+            self.matches_from(true, s.chars(), 0) == MatchResult::Match
+        }
+        // vaguely based off glob's matches_from
+        fn matches_from(
+            &self,
+            mut follows_separator: bool,
+            mut path: std::str::Chars,
+            i: usize,
+        ) -> MatchResult {
+            for (ti, &token) in self.tokens[i..].iter().enumerate() {
+                match token {
+                    Token::Star | Token::DoubleStar => {
+                        // Empty match
+                        match self.matches_from(follows_separator, path.clone(), i + ti + 1) {
+                            MatchResult::SubPatternDoesntMatch => {} // keep trying
+                            m => return m,
+                        }
+
+                        while let Some(c) = path.next() {
+                            follows_separator = c == '.';
+                            match token {
+                                Token::DoubleStar if !follows_separator => continue,
+                                Token::Star if follows_separator => {
+                                    return MatchResult::SubPatternDoesntMatch
+                                }
+                                _ => {}
+                            }
+                            match self.matches_from(follows_separator, path.clone(), i + ti + 1) {
+                                MatchResult::SubPatternDoesntMatch => {} // keep trying
+                                m => return m,
+                            }
+                        }
+                    }
+                    Token::Char(exp) => {
+                        let Some(c) = path.next() else { return MatchResult::EntirePatternDoesntMatch };
+                        if c != exp {
+                            return MatchResult::SubPatternDoesntMatch;
+                        }
+                        follows_separator = c == '.';
+                    }
+                }
+            }
+
+            // Iter is fused.
+            if path.next().is_none() {
+                MatchResult::Match
+            } else {
+                MatchResult::SubPatternDoesntMatch
+            }
+        }
+    }
+
+    #[derive(PartialEq, Eq, Debug)]
+    enum MatchResult {
+        Match,
+        SubPatternDoesntMatch,
+        EntirePatternDoesntMatch,
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_pattern() {
+        let pattern: ModulePattern = "x.bar.foo_*.a".parse().unwrap();
+        assert!(pattern.matches("x.bar.foo_asdf.a"));
+        assert!(pattern.matches("x.bar.foo_bazzzz.a"));
+        assert!(pattern.matches("x.bar.foo_.a"));
+        assert!(!pattern.matches("x.bar.foo_"));
+        assert!(!pattern.matches("x.bar.foo_quxxx"));
+        assert!(!pattern.matches("foo_b.a"));
+
+        let pattern: ModulePattern = "**.foo.**".parse().unwrap();
+        assert!(pattern.matches("ba.bazzz.foo.quux"));
+
+        let pattern: ModulePattern = "*.foo.**".parse().unwrap();
+        assert!(pattern.matches("ba.foo.baz.quux"));
+        assert!(pattern.matches("asdf.foo.barrr"));
+
+        let pattern: ModulePattern = "foo.**".parse().unwrap();
+        assert!(pattern.matches("foo.baaar.qx"));
+        assert!(!pattern.matches("asdf.foo.brrrr"));
+    }
 }
